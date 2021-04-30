faada5e7104523658dd6f94bb2292d2025dd0fc6
Deepak <deepakrajan.grea@gmail.com>
2021-04-30 15:43:38 +0530

    react-deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Between Rules Lines.
         </p>
         <a
           className="App-link"
